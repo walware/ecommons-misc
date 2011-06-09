719df8ef708715dab0e3b103656b31d34698ea5e
Stephan Wahlbrink <sw@wahlbrink.eu>
2011-06-09 11:30:24 +0200

    [ECommons-Databinding] Added decimal validator
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

---
+++ de.walware.ecommons.coremisc/src/de/walware/ecommons/databinding/DecimalValidator.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2011 WalWare/StatET-Project (www.walware.de/goto/statet)
+ * and others. All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Stephan Wahlbrink - initial API and implementation
+ *******************************************************************************/
+
+package de.walware.ecommons.databinding;
+
+import java.text.ParsePosition;
+
+import com.ibm.icu.text.NumberFormat;
+
+import org.eclipse.core.databinding.validation.IValidator;
+import org.eclipse.core.databinding.validation.ValidationStatus;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+
+/**
+ * Validator for decimals.
+ */
+public class DecimalValidator implements IValidator {
+	
+	
+	private final NumberFormat fFormatter;
+	private final double fMin;
+	private final double fMax;
+	private final boolean fAllowEmpty;
+	private final String fMessage;
+	
+	
+	public DecimalValidator(final double min, final double max, final String message) {
+		this(min, max, false, message);
+	}
+	
+	public DecimalValidator(final double min, final double max, final boolean allowEmpty, final String message) {
+		fMin = min;
+		fMax = max;
+		fAllowEmpty = allowEmpty;
+		fMessage = message;
+		fFormatter = NumberFormat.getNumberInstance();
+		fFormatter.setParseIntegerOnly(false);
+	}
+	
+	
+	public IStatus validate(final Object value) {
+		if (value instanceof String) {
+			final String s = ((String) value).trim();
+			if (fAllowEmpty && s.length() == 0) {
+				return Status.OK_STATUS;
+			}
+			final ParsePosition result = new ParsePosition(0);
+			final Number number = fFormatter.parse(s, result);
+			if (result.getIndex() == s.length() && result.getErrorIndex() < 0) {
+				final double n = number.doubleValue();
+				if (n >= fMin && n <= fMax) {
+					return Status.OK_STATUS;
+				}
+				// return range message
+			}
+		}
+		return ValidationStatus.error(fMessage);
+	}
+	
+}
--- de.walware.ecommons.coremisc/src/de/walware/ecommons/databinding/NumberValidator.java
+++ de.walware.ecommons.coremisc/src/de/walware/ecommons/databinding/NumberValidator.java
@@ -11,60 +11,22 @@
 
 package de.walware.ecommons.databinding;
 
-import java.text.ParsePosition;
-
-import com.ibm.icu.text.NumberFormat;
-
-import org.eclipse.core.databinding.validation.IValidator;
-import org.eclipse.core.databinding.validation.ValidationStatus;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-
 
 /**
  * Validator for integers.
+ * 
+ * @deprecated replaced by {@link IntegerValidator}
  */
-public class NumberValidator implements IValidator {
-	
-	
-	private final NumberFormat fFormatter;
-	private final int fMin;
-	private final int fMax;
-	private final boolean fAllowEmpty;
-	private final String fMessage;
+@Deprecated
+public class NumberValidator extends IntegerValidator {
 	
 	
 	public NumberValidator(final int min, final int max, final String message) {
-		this(min, max, false, message);
+		super(min, max, false, message);
 	}
 	
 	public NumberValidator(final int min, final int max, final boolean allowEmpty, final String message) {
-		fMin = min;
-		fMax = max;
-		fAllowEmpty = allowEmpty;
-		fMessage = message;
-		fFormatter = NumberFormat.getIntegerInstance();
-		fFormatter.setParseIntegerOnly(true);
-	}
-	
-	
-	public IStatus validate(final Object value) {
-		if (value instanceof String) {
-			final String s = ((String) value).trim();
-			if (fAllowEmpty && s.length() == 0) {
-				return Status.OK_STATUS;
-			}
-			final ParsePosition result = new ParsePosition(0);
-			final Number number = fFormatter.parse(s, result);
-			if (result.getIndex() == s.length() && result.getErrorIndex() < 0) {
-				final int n = number.intValue();
-				if (n >= fMin && n <= fMax) {
-					return Status.OK_STATUS;
-				}
-				// return range message
-			}
-		}
-		return ValidationStatus.error(fMessage);
+		super(min, max, allowEmpty, message);
 	}
 	
 }

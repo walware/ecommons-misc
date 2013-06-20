ad87b8d2d92d43e7ef4e31c2916eeb3b9d392485
Stephan Wahlbrink <sw@wahlbrink.eu>
2013-06-20 10:33:25 +0200

    [DataBinding] Added validator for long integers
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

---
+++ de.walware.ecommons.coremisc/src/de/walware/ecommons/databinding/LongValidator.java
@@ -0,0 +1,70 @@
+/*******************************************************************************
+ * Copyright (c) 2007-2013 WalWare/StatET-Project (www.walware.de/goto/statet)
+ * and others. All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Stephan Wahlbrink - initial API and implementation
+ *******************************************************************************/
+
+package de.walware.ecommons.databinding;
+
+import java.text.ParsePosition;
+
+import com.ibm.icu.text.NumberFormat;
+
+import org.eclipse.core.databinding.validation.IValidator;
+import org.eclipse.core.databinding.validation.ValidationStatus;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+
+
+/**
+ * Validator for long integers.
+ */
+public class LongValidator implements IValidator {
+	
+	
+	private final NumberFormat fFormatter;
+	private final long fMin;
+	private final long fMax;
+	private final boolean fAllowEmpty;
+	private final String fMessage;
+	
+	
+	public LongValidator(final long min, final long max, final String message) {
+		this(min, max, false, message);
+	}
+	
+	public LongValidator(final long min, final long max, final boolean allowEmpty, final String message) {
+		fMin = min;
+		fMax = max;
+		fAllowEmpty = allowEmpty;
+		fMessage = message;
+		fFormatter = NumberFormat.getIntegerInstance();
+		fFormatter.setParseIntegerOnly(true);
+	}
+	
+	
+	public IStatus validate(final Object value) {
+		if (value instanceof String) {
+			final String s = ((String) value).trim();
+			if (fAllowEmpty && s.length() == 0) {
+				return Status.OK_STATUS;
+			}
+			final ParsePosition result = new ParsePosition(0);
+			final Number number = fFormatter.parse(s, result);
+			if (result.getIndex() == s.length() && result.getErrorIndex() < 0) {
+				final long n = number.longValue();
+				if (n >= fMin && n <= fMax) {
+					return Status.OK_STATUS;
+				}
+				// return range message
+			}
+		}
+		return ValidationStatus.error(fMessage);
+	}
+	
+}

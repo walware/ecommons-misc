ecbc9b21770394002678f85617dd7f95fc1d219e
Stephan Wahlbrink <sw@wahlbrink.eu>
2014-03-02 18:39:18 +0100

    [ECommons-Resources] Enhanced utils for workspace projects
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

---
+++ de.walware.ecommons.coremisc/src/de/walware/ecommons/resources/AbstractProjectNature.java
@@ -0,0 +1,138 @@
+/*=============================================================================#
+ # Copyright (c) 2005-2014 Stephan Wahlbrink (WalWare.de) and others.
+ # All rights reserved. This program and the accompanying materials
+ # are made available under the terms of the Eclipse Public License v1.0
+ # which accompanies this distribution, and is available at
+ # http://www.eclipse.org/legal/epl-v10.html
+ # 
+ # Contributors:
+ #     Stephan Wahlbrink - initial API and implementation
+ #=============================================================================*/
+
+package de.walware.ecommons.resources;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+import org.eclipse.core.resources.ProjectScope;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences.IPreferenceChangeListener;
+import org.eclipse.core.runtime.preferences.IScopeContext;
+import org.eclipse.core.runtime.preferences.InstanceScope;
+
+import de.walware.ecommons.preferences.IPreferenceAccess;
+import de.walware.ecommons.preferences.Preference;
+import de.walware.ecommons.preferences.PreferencesUtil;
+
+
+public class AbstractProjectNature implements IProjectNature, IPreferenceAccess {
+	
+	
+	private IProject project;
+	
+	private IScopeContext[] contexts;
+	
+	
+	public AbstractProjectNature() {
+		super();
+	}
+	
+	
+	@Override
+	public void setProject(final IProject project) {
+		this.project = project;
+		this.contexts = createPrefContexts(true);
+	}
+	
+	@Override
+	public final IProject getProject() {
+		return this.project;
+	}
+	
+	@Override
+	public void configure() throws CoreException {
+		addBuilders();
+	}
+	
+	@Override
+	public void deconfigure() throws CoreException {
+		removeBuilders();
+	}
+	
+	protected void addBuilders() throws CoreException {
+	}
+	
+	protected void removeBuilders() throws CoreException {
+	}
+	
+/*-- IPreferenceAccess -------------------------------------------------------*/
+	
+	private IScopeContext[] createPrefContexts(final boolean inheritInstanceSettings) {
+		return (inheritInstanceSettings) ?
+				new IScopeContext[] {
+					new ProjectScope(getProject()),
+					InstanceScope.INSTANCE,
+					DefaultScope.INSTANCE,
+				} :
+				new IScopeContext[] {
+					new ProjectScope(getProject()),
+					DefaultScope.INSTANCE,
+				};
+	}
+	
+	@Override
+	public <T> T getPreferenceValue(final Preference<T> key) {
+		return PreferencesUtil.getPrefValue(this.contexts, key);
+	}
+	
+	@Override
+	public IEclipsePreferences[] getPreferenceNodes(final String nodeQualifier) {
+		return PreferencesUtil.getRelevantNodes(nodeQualifier, this.contexts);
+	}
+	
+	@Override
+	public IScopeContext[] getPreferenceContexts() {
+		return this.contexts;
+	}
+	
+	@Override
+	public void addPreferenceNodeListener(final String nodeQualifier, final IPreferenceChangeListener listener) {
+		int i = this.contexts.length-2;
+		while (i >= 0) {
+			final IEclipsePreferences node = this.contexts[i--].getNode(nodeQualifier);
+			if (node != null) {
+				node.addPreferenceChangeListener(listener);
+			}
+		}
+	}
+	
+	@Override
+	public void removePreferenceNodeListener(final String nodeQualifier, final IPreferenceChangeListener listener) {
+		int i = this.contexts.length-2;
+		while (i >= 0) {
+			final IEclipsePreferences node = this.contexts[i--].getNode(nodeQualifier);
+			if (node != null) {
+				node.removePreferenceChangeListener(listener);
+			}
+		}
+	}
+	
+	public IScopeContext getProjectContext() {
+		return this.contexts[0];
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return getProject().hashCode() + 1;
+	}
+	
+	@Override
+	public boolean equals(final Object obj) {
+		return (this == obj || (obj != null
+				&& getClass().equals(obj.getClass())
+				&& getProject().equals(((AbstractProjectNature) obj).getProject()) ));
+	}
+	
+}
--- de.walware.ecommons.coremisc/src/de/walware/ecommons/resources/ProjectUtil.java
+++ de.walware.ecommons.coremisc/src/de/walware/ecommons/resources/ProjectUtil.java
@@ -11,38 +11,123 @@
 
 package de.walware.ecommons.resources;
 
+import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProjectDescription;
 
 
 public class ProjectUtil {
 	
 	
-	public static IProjectDescription appendNature(final IProjectDescription description, final String id) {
-		final String[] prevNatures = description.getNatureIds();
-		for (int i = 0; i < prevNatures.length; i++) {
-			if (prevNatures[i].equals(id)) {
-				return description;
+	private static int indexOf(final String[] ids, final String id) {
+		for (int idx= 0; idx < ids.length; idx++) {
+			if (ids[idx].equals(id)) {
+				return idx;
 			}
 		}
-		final String[] newNatures = new String[prevNatures.length + 1];
-		System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
-		newNatures[prevNatures.length] = id;
-		description.setNatureIds(newNatures);
-		return description;
+		return -1;
 	}
 	
-	public static IProjectDescription removeNature(final IProjectDescription description, final String id) {
-		final String[] prevNatures = description.getNatureIds();
-		for (int i = 0; i < prevNatures.length; i++) {
-			if (prevNatures[i].equals(id)) {
-				final String[] newNatures = new String[prevNatures.length - 1];
-				System.arraycopy(prevNatures, 0, newNatures, 0, i);
-				System.arraycopy(prevNatures, i+1, newNatures, i, prevNatures.length-i-1);
-				description.setNatureIds(newNatures);
-				break;
+	private static int indexOf(final ICommand[] commands, final String id) {
+		for (int idx= 0; idx < commands.length; idx++) {
+			if (commands[idx].getBuilderName().equals(id)) {
+				return idx;
 			}
 		}
-		return description;
+		return -1;
+	}
+	
+	/**
+	 * Adds the specified project nature to the project description.
+	 * 
+	 * @param description
+	 * @param natureId id of the nature to add
+	 * @return <code>true</code> if the project description is changed by this method
+	 */
+	public static boolean addNature(final IProjectDescription description,
+			final String natureId) {
+		final String[] prevNatures= description.getNatureIds();
+		final int idx= indexOf(prevNatures, natureId);
+		
+		if (idx == -1) {
+			final String[] newNatures= new String[prevNatures.length + 1];
+			System.arraycopy(prevNatures, 0, newNatures, 0, prevNatures.length);
+			newNatures[prevNatures.length]= natureId;
+			description.setNatureIds(newNatures);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Removes the specified project nature to the project description.
+	 * 
+	 * @param description
+	 * @param natureId id of the nature to remove
+	 * @return <code>true</code> if the project description is changed by this method
+	 */
+	public static boolean removeNature(final IProjectDescription description,
+			final String natureId) {
+		final String[] prevNatures= description.getNatureIds();
+		final int idx= indexOf(prevNatures, natureId);
+		
+		if (idx >= 0) {
+			final String[] newNatures= new String[prevNatures.length - 1];
+			System.arraycopy(prevNatures, 0, newNatures, 0, idx);
+			System.arraycopy(prevNatures, idx + 1, newNatures, idx, prevNatures.length - idx - 1);
+			description.setNatureIds(newNatures);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Adds the specified project builder to the project description.
+	 * 
+	 * @param description
+	 * @param builderId the id of the builder to add
+	 * @return <code>true</code> if the project description is changed by this method
+	 */
+	public static boolean addBuilder(final IProjectDescription description,
+			final String builderId) {
+		final ICommand[] prevCommands= description.getBuildSpec();
+		final int idx= indexOf(prevCommands, builderId);
+		
+		if (idx == -1) {
+			final ICommand newCommand= description.newCommand();
+			newCommand.setBuilderName(builderId);
+			final ICommand[] newCommands= new ICommand[prevCommands.length+1];
+			System.arraycopy(prevCommands, 0, newCommands, 0, prevCommands.length);
+			newCommands[prevCommands.length]= newCommand;
+			description.setBuildSpec(newCommands);
+			return true;
+		}
+		
+		return false;
+	}
+	
+	/**
+	 * Adds the specified project builder to the project description.
+	 * 
+	 * @param description
+	 * @param builderId the id of the builder to add
+	 * @return <code>true</code> if the project description is changed by this method
+	 */
+	public static boolean removeBuilder(final IProjectDescription description,
+			final String builderId) {
+		final ICommand[] prevCommands= description.getBuildSpec();
+		final int idx= indexOf(prevCommands, builderId);
+		
+		if (idx >= 0) {
+			final ICommand[] newCommands= new ICommand[prevCommands.length - 1];
+			System.arraycopy(prevCommands, 0, newCommands, 0, idx);
+			System.arraycopy(prevCommands, idx + 1, newCommands, idx, prevCommands.length - idx - 1);
+			description.setBuildSpec(newCommands);
+			return true;
+		}
+		
+		return false;
 	}
 	
 	

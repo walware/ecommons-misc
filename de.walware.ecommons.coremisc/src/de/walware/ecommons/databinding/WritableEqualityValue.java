bad8c5c5880a459ba36f2e471d85692838cdc3d5
Stephan Wahlbrink <sw@wahlbrink.eu>
2015-04-17 17:27:12 +0200

    [Databinding] Added WritableEqualityValue using equality instead of identity when checking if value changed
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

---
+++ de.walware.ecommons.coremisc/src/de/walware/ecommons/databinding/WritableEqualityValue.java
@@ -0,0 +1,92 @@
+/*=============================================================================#
+ # Copyright (c) 2006-2015 IBM Corporation and others.
+ # All rights reserved. This program and the accompanying materials
+ # are made available under the terms of the Eclipse Public License v1.0
+ # which accompanies this distribution, and is available at
+ # http://www.eclipse.org/legal/epl-v10.html
+ # 
+ # Contributors:
+ #     IBM Corporation - initial API and implementation of WritableValue
+ #     Stephan Wahlbrink - initial WritableEqualityValue
+ #=============================================================================*/
+
+package de.walware.ecommons.databinding;
+
+import org.eclipse.core.databinding.observable.Diffs;
+import org.eclipse.core.databinding.observable.Realm;
+import org.eclipse.core.databinding.observable.value.AbstractObservableValue;
+
+
+public class WritableEqualityValue extends AbstractObservableValue {
+	
+	
+	private final Object valueType;
+	
+	private Object value = null;
+	
+	
+	/**
+	 * Constructs a new instance with the default realm, a <code>null</code>
+	 * value type, and a <code>null</code> value.
+	 */
+	public WritableEqualityValue() {
+		this(null, null);
+	}
+	
+	/**
+	 * Constructs a new instance with the default realm.
+	 *
+	 * @param initialValue
+	 *            can be <code>null</code>
+	 * @param valueType
+	 *            can be <code>null</code>
+	 */
+	public WritableEqualityValue(Object initialValue, Class valueType) {
+		this(Realm.getDefault(), initialValue, valueType);
+	}
+	
+	/**
+	 * Constructs a new instance with the provided <code>realm</code>, a
+	 * <code>null</code> value type, and a <code>null</code> initial value.
+	 *
+	 * @param realm
+	 */
+	public WritableEqualityValue(Realm realm) {
+		this(realm, null, null);
+	}
+	
+	/**
+	 * Constructs a new instance.
+	 *
+	 * @param realm
+	 * @param initialValue
+	 *            can be <code>null</code>
+	 * @param valueType
+	 *            can be <code>null</code>
+	 */
+	public WritableEqualityValue(Realm realm, Object initialValue, Class valueType) {
+		super(realm);
+		this.valueType = valueType;
+		this.value = initialValue;
+	}
+	
+	
+	@Override
+	public Object getValueType() {
+		return valueType;
+	}
+	
+	@Override
+	public Object doGetValue() {
+		return value;
+	}
+	
+	@Override
+	public void doSetValue(final Object value) {
+		final Object oldValue= doGetValue();
+		if (value != oldValue && (value == null || !value.equals(oldValue))) {
+			fireValueChange(Diffs.createValueDiff(this.value, this.value= value));
+		}
+	}
+	
+}

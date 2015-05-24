5a17055d9220b17c5e1c9c539aac8dba24424906
Stephan Wahlbrink <sw@wahlbrink.eu>
2015-05-24 20:45:56 +0200

    [Runtime] Added PathUtils
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

---
+++ de.walware.ecommons.coremisc/src/de/walware/ecommons/runtime/core/utils/PathUtils.java
@@ -0,0 +1,33 @@
+/*=============================================================================#
+ # Copyright (c) 2015 Stephan Wahlbrink (WalWare.de) and others.
+ # All rights reserved. This program and the accompanying materials
+ # are made available under the terms of the Eclipse Public License v1.0
+ # which accompanies this distribution, and is available at
+ # http://www.eclipse.org/legal/epl-v10.html
+ # 
+ # Contributors:
+ #     Stephan Wahlbrink - initial API and implementation
+ #=============================================================================*/
+
+package de.walware.ecommons.runtime.core.utils;
+
+import org.eclipse.core.runtime.IPath;
+
+
+public class PathUtils {
+	
+	
+	public static boolean isValid(final IPath path) {
+		for (int i= 0; i < path.segmentCount(); i++) {
+			if (!path.isValidSegment(path.segment(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static IPath check(final IPath path) {
+		return (path != null && isValid(path)) ? path : null;
+	}
+	
+}

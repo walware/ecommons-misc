84632bf07ddc85e6811258ac9fe310e53263c5a1
Stephan Wahlbrink <sw@wahlbrink.eu>
2015-07-03 12:28:52 +0200

    [Databinding] Added StringTrimConverter
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

---
+++ de.walware.ecommons.databinding.core/src/de/walware/ecommons/databinding/core/conversion/StringTrimConverter.java
@@ -0,0 +1,42 @@
+/*=============================================================================#
+ # Copyright (c) 2015 Stephan Wahlbrink (WalWare.de) and others.
+ # All rights reserved. This program and the accompanying materials
+ # are made available under the terms of the Eclipse Public License v1.0
+ # which accompanies this distribution, and is available at
+ # http://www.eclipse.org/legal/epl-v10.html
+ # 
+ # Contributors:
+ #     Stephan Wahlbrink - initial API and implementation
+ #=============================================================================*/
+
+package de.walware.ecommons.databinding.core.conversion;
+
+import org.eclipse.core.databinding.conversion.IConverter;
+
+
+public class StringTrimConverter implements IConverter {
+	
+	
+	public static final IConverter INSTANCE= new StringTrimConverter();
+	
+	
+	public StringTrimConverter() {
+	}
+	
+	
+	@Override
+	public Object getFromType() {
+		return String.class;
+	}
+	
+	@Override
+	public Object getToType() {
+		return String.class;
+	}
+	
+	@Override
+	public Object convert(final Object fromObject) {
+		return ((String) fromObject).trim();
+	}
+	
+}

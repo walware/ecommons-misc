a21039cbd8532491c6d025d6032fbee1a6dc2b19
Stephan Wahlbrink <sw@wahlbrink.eu>
2015-10-09 14:41:22 +0200

    [Text] Added ITextRegion
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

---
+++ de.walware.ecommons.text.core/src/de/walware/ecommons/text/core/TextRegion.java
@@ -0,0 +1,87 @@
+/*=============================================================================#
+ # Copyright (c) 2015 Stephan Wahlbrink (WalWare.de) and others.
+ # All rights reserved. This program and the accompanying materials
+ # are made available under the terms of the Eclipse Public License v1.0
+ # which accompanies this distribution, and is available at
+ # http://www.eclipse.org/legal/epl-v10.html
+ # 
+ # Contributors:
+ #     Stephan Wahlbrink - initial API and implementation
+ #=============================================================================*/
+
+package de.walware.ecommons.text.core;
+
+import org.eclipse.jface.text.IRegion;
+
+
+public class TextRegion implements ITextRegion {
+	
+	
+	private final int beginOffset;
+	private final int endOffset;
+	
+	
+	public TextRegion(final int beginOffset, final int endOffset) {
+		if (beginOffset > endOffset) {
+			throw new IllegalArgumentException("beginOffset > endOffset: beginOffset= " + beginOffset + ", endOffset= " + endOffset); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		this.beginOffset= beginOffset;
+		this.endOffset= endOffset;
+	}
+	
+	public TextRegion(final ITextRegion region) {
+		this(region.getOffset(), region.getEndOffset());
+	}
+	
+	public TextRegion(final IRegion region) {
+		this(region.getOffset(), region.getLength() - region.getOffset());
+	}
+	
+	
+	@Override
+	public final int getOffset() {
+		return this.beginOffset;
+	}
+	
+	@Override
+	public final int getEndOffset() {
+		return this.endOffset;
+	}
+	
+	@Override
+	public final int getLength() {
+		return this.endOffset - this.beginOffset;
+	}
+	
+	
+	@Override
+	public int hashCode() {
+		return (this.beginOffset << 24) | (getLength() << 16);
+	}
+	
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj instanceof IRegion) {
+			final IRegion other= (IRegion) obj;
+			return (this.beginOffset == other.getOffset()
+					&& getLength() == other.getLength());
+		}
+		return false;
+	}
+	
+	
+	@Override
+	public String toString() {
+		final StringBuilder sb= new StringBuilder();
+		sb.append('[');
+		sb.append(this.beginOffset);
+		sb.append(',');
+		sb.append(this.endOffset);
+		sb.append(')');
+		return sb.toString();
+	}
+	
+}

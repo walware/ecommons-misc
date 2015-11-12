9c3557ac841009af7be75c11b32ec51464b51175
Stephan Wahlbrink <sw@wahlbrink.eu>
2015-11-12 15:18:59 +0100

    [Text] Added text.core.sections / IDocContentSections (moved from LTK)
    Signed-off-by: Stephan Wahlbrink <sw@wahlbrink.eu>

--- de.walware.ecommons.text.core/src/de/walware/ecommons/text/core/util/TextUtils.java
+++ de.walware.ecommons.text.core/src/de/walware/ecommons/text/core/util/TextUtils.java
@@ -11,9 +11,19 @@
 
 package de.walware.ecommons.text.core.util;
 
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.BadPartitioningException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IDocumentExtension3;
 import org.eclipse.jface.text.ISynchronizable;
+import org.eclipse.jface.text.ITypedRegion;
+
+import de.walware.ecommons.text.core.sections.IDocContentSections;
 
 
+/**
+ * @since de.walware.ecommons.text 1.0
+ */
 public final class TextUtils {
 	
 	
@@ -21,6 +31,27 @@ public final class TextUtils {
 		final Object lockObject= (o instanceof ISynchronizable) ?
 				((ISynchronizable) o).getLockObject() : null;
 		return (lockObject != null) ? lockObject : o;
+	}
+	
+	
+	/**
+	 * @since de.walware.ecommons.text 1.1
+	 */
+	public static String getContentType(final IDocument document, final IDocContentSections contentInfo,
+			final int offset, final boolean preferOpenPartition)
+			throws BadPartitioningException, BadLocationException {
+		return ((IDocumentExtension3) document).getContentType(contentInfo.getPartitioning(),
+				offset, preferOpenPartition );
+	}
+	
+	/**
+	 * @since de.walware.ecommons.text 1.1
+	 */
+	public static ITypedRegion getPartition(final IDocument document, final IDocContentSections contentInfo,
+			final int offset, final boolean preferOpenPartition)
+					throws BadPartitioningException, BadLocationException {
+		return ((IDocumentExtension3) document).getPartition(contentInfo.getPartitioning(),
+				offset, preferOpenPartition );
 	}
 	
 	
